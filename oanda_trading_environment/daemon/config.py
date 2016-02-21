4ac955d7d3173fc1c1671a52b97cd1eeb32abc55
hootnot <f.brekeveld@gmail.com>
2016-02-21 15:18:41 +0100

    PEP8 and PEP257 compliancy modifications

--- oanda_trading_environment/daemon/config.py
+++ oanda_trading_environment/daemon/config.py
@@ -1,7 +1,6 @@
-r""" Config related functionality.
+"""Config related functionality.
 
 This module provides access to configuration files in YAML markup
-
 """
 
 import yaml
@@ -10,23 +9,28 @@ from os import path
 
 
 class NoDataError(Exception):
-    """ No config data available """
+    """No config data available."""
 
 
 class Config(object):
+    """Config - handle configfiles in yaml markup.
+
+    config data is accessible via overloaded __getitem__
+
+    Config data can be loaded from a config file user the load(..) method or
+    by passing it data as a dict when creating a Config instance.
+
+    All attributes in the config having 'path' or 'file' in their name will be
+    altered automatically by prepending the prefix if it concerns relative
+    paths
+
+    If no data is available, a NoDataError will be raised
+    """
 
     def __init__(self, configdata=None, prefix=None, tokenseparator="::"):
-        """
-           Config - handle configfiles in yaml markup
-           config data is accessible via overloaded __getitem__
+        """Create a Config instance.
 
-           Config data can be loaded from a config file user the load(..) method or
-           by passing it data as a dict when creating a Config instance.
-
-           All attributes in the config having 'path' or 'file' in their name will be
-           altered automatically by prepending the prefix if it concerns relative paths
-
-           If no data is available, a NoDataError will be raised
+        If no data is available, a NoDataError will be raised
         """
         self.data = None
         self.prefix = prefix
@@ -35,39 +39,42 @@ class Config(object):
             self.data = configdata
 
     def load(self, filename):
-        """
-           read configdata from file :filename
+        """load - read configdata from config file.
+
+        Parameters
+        ----------
+        filename : string
+            path to the configuration file
         """
         self.filename = filename
         with open(filename) as CFG:
             self.data = yaml.load(CFG.read())
 
     def __getitem__(self, k):
-        """
-           return configuration item by key
+        """return configuration item by key.
 
-           >>> from ... import Config
-           >>> c = Config()
-           >>> c.load(CONFIG_FILE)
-           >>> print c["daemon::user"]
-           oanda
-           >>> print c["daemon::logfile"]
-           var/log/OANDA/OANDAd.log
-           >>> print c["daemon::pidfile"]
-           /var/run/OANDA/OANDAd.pid
+        >>> from ... import Config
+        >>> c = Config()
+        >>> c.load(CONFIG_FILE)
+        >>> print c["daemon::user"]
+        oanda
+        >>> print c["daemon::logfile"]
+        var/log/OANDA/OANDAd.log
+        >>> print c["daemon::pidfile"]
+        /var/run/OANDA/OANDAd.pid
 
-           The same, but now with a prefix passed:
+        The same, but now with a prefix passed:
 
-           >>> c2 = Config(prefix="/somewhere/else")
-           >>> c2.load(CONFIG_FILE)
-           >>> print c["daemon::user"]
-           oanda
-           >>> print c["daemon::logfile"]
-           /somewhere/else/var/log/OANDA/OANDAd.log
-           >>> print c["daemon::pidfile"]
-           /var/run/OANDA/OANDAd.pid
+        >>> c2 = Config(prefix="/somewhere/else")
+        >>> c2.load(CONFIG_FILE)
+        >>> print c["daemon::user"]
+        oanda
+        >>> print c["daemon::logfile"]
+        /somewhere/else/var/log/OANDA/OANDAd.log
+        >>> print c["daemon::pidfile"]
+        /var/run/OANDA/OANDAd.pid
 
-           the relative path has got the prefix prepended.
+        the relative path has got the prefix prepended.
         """
         def _gv_(data, keywords):
             # recursively traverse the data structure untile the last key
