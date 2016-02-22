1ddb243e78cd0e27d8721c15888e986eaea44900
hootnot <f.brekeveld@gmail.com>
2016-02-22 19:11:08 +0100

    tests added - test__abspath - test__relpath

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 testcfg = None
+OAPREFIX = "/tmp"
 
 
 def logit(f):
@@ -19,7 +20,7 @@ def logit(f):
 class Test_Config(unittest.TestCase):
     def setUp(self):
         global testcfg
-        testcfg = config.Config()
+        testcfg = config.Config(prefix=OAPREFIX)
         testcfg.load("etc/OANDA/config/OANDAd.cfg")
 
     def test_openfailure(self):
@@ -55,6 +56,22 @@ class Test_Config(unittest.TestCase):
         self.assertEqual({"from": testcfg['schedule::from'],
                           }, {
            'from': 'sunday, 23:00',
+        })
+
+    def test__abspath(self):
+        """ TEST: get parameter with absolute path"
+        """
+        self.assertEqual({"pidfile": testcfg['daemon::pidfile'],
+                          }, {
+           'pidfile': '/var/run/OANDA/OANDAd.pid'
+        })
+
+    def test__relpath(self):
+        """ TEST: get parameter with relative path"
+        """
+        self.assertEqual({"logfile": testcfg['daemon::logfile'],
+                          }, {
+           'logfile': os.path.join(OAPREFIX, 'var/log/OANDA/OANDAd.log')
         })
 
     def test__timeframes(self):
